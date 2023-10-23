6102512a4fb2ec9c49d488e4ebcbdb12b78c83ee
shljessie <sl994@cornell.edu>
2023-10-22 20:30:34 -0700

    update langchat

--- chat/langchat.py
+++ chat/langchat.py
@@ -5,20 +5,85 @@ from langchain import LLMChain
 from langchain.prompts.prompt import PromptTemplate
 from langchain.memory import ConversationBufferMemory
 from langchain.llms import HuggingFacePipeline
+import transformers
+from torch import cuda, bfloat16
 
 model_name_or_path = "../Llama-2-7b-chat-hf"
 model_basename = "Llama-2-7b-chat-hf"
 use_triton = False
 
+device = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu'
+
 
 tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
 
-model = AutoGPTQForCausalLM.from_quantized(model_name_or_path,
-        model_basename=model_basename,
-        use_safetensors=True,
-        trust_remote_code=True,
-        use_triton=use_triton,
-        quantize_config=None)
+quant_config = transformers.BitsAndBytesConfig(
+
+    load_in_4bit=True,
+
+    bnb_4bit_quant_type='nf4',
+
+    bnb_4bit_use_double_quant=True,
+
+    bnb_4bit_compute_dtype=bfloat16
+
+)
+
+model_config = transformers.AutoConfig.from_pretrained(
+
+    model_basename,
+
+    use_auth_token='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'
+
+)
+
+model = transformers.AutoModelForCausalLM.from_pretrained(
+
+    model_basename,
+
+    trust_remote_code=True,
+
+    config=model_config,
+
+    quantization_config=quant_config,
+
+    use_auth_token='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'
+
+)
+
+model.eval()
+
+print(f"Model loaded on {device}")
+
+tokenizer = transformers.AutoTokenizer.from_pretrained(
+
+    model_basename,
+
+    use_auth_token='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'
+
+)
+
+model = transformers.AutoModelForCausalLM.from_pretrained(
+
+    model_basename,
+
+    trust_remote_code=True,
+
+    config=model_config,
+
+    quantization_config=quant_config,
+
+    use_auth_token='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'
+
+)
+
+
+# model = AutoGPTQForCausalLM.from_quantized(model_name_or_path,
+#         model_basename=model_basename,
+#         use_safetensors=True,
+#         trust_remote_code=True,
+#         use_triton=use_triton,
+#         quantize_config=None)
 
 
 logging.set_verbosity(logging.CRITICAL)
