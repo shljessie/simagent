91d3852c6509a73393591eae5e11e63af411d458
shljessie <sl994@cornell.edu>
2023-10-25 05:22:33 -0700

    update name

--- chat/web.py
+++ chat/web.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-import your_script  # Assuming your_script contains the defined code
+import llama2  # Assuming your_script contains the defined code
 
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ def process_text():
     
     # Here you should call the function where you process the text
     # assuming process_text is that function and it returns the processed text
-    result = your_script.process_text(text)
+    result = llama2.process_text(text)
     
     return jsonify(result)
 
