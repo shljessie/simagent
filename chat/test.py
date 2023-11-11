b196f0f6cf9c550c957c789120cfa46f05840c5a
shljessie <sl994@cornell.edu>
2023-11-11 15:37:09 -0800

    updatemodel name

--- chat/test.py
+++ chat/test.py
@@ -9,7 +9,7 @@ model_id = "../Llama-2-7b-chat-hf"
 HF_ACCESS_TOKEN = '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'
 
 model = AutoModelForCausalLM.from_pretrained(model_id, use_auth_token=HF_ACCESS_TOKEN, torch_dtype=torch.float16, device_map="auto")
-tokenizer = AutoTokenizer.from_pretrained(model, use_auth_token=HF_ACCESS_TOKEN,)
+tokenizer = AutoTokenizer.from_pretrained(model_id, use_auth_token=HF_ACCESS_TOKEN,)
 pipeline = transformers.pipeline(
     "text-generation",
     model=model,
