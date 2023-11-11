56f85079be8ebbf46b3530d84592977a5a97ab54
shljessie <sl994@cornell.edu>
2023-11-11 15:49:39 -0800

    updateprompt

--- chat/test.py
+++ chat/test.py
@@ -19,8 +19,20 @@ pipeline = transformers.pipeline(
     token = HF_ACCESS_TOKEN
 )
 
+
+prompt = """
+
+<s>[INST] <<SYS>>
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+<</SYS>>
+
+There's a llama in my garden 😱 What should I do? [/INST]
+
+"""
 sequences = pipeline(
-    'I liked "Breaking Bad" and "Band of Brothers". Do you have any recommendations of other shows I might like?\n',
+    prompt,
     do_sample=True,
     top_k=10,
     num_return_sequences=1,
