9b1d3ed579befde54733445600cc3b12d74b081b
shljessie <sl994@cornell.edu>
2023-11-11 16:01:53 -0800

    update convo history

--- chat/test.py
+++ chat/test.py
@@ -20,7 +20,7 @@ pipeline = transformers.pipeline(
 )
 
 
-prompt = """
+initial_prompt = """
 
 <s>[INST] <<SYS>>
 You are the Persona. Always act as you are the persona.
@@ -30,13 +30,44 @@ Persona:  My name is Jack i like to party.  my major is business. i am in colleg
 What is your name? [/INST]
 
 """
+#initial sequence
 sequences = pipeline(
+    initial_prompt,
+    do_sample=True,
+    top_k=10,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+    max_length=250,
+)
+
+def generate(conversational_history, prompt):
+  """Take in convo history and generate response to last utterance
+
+  conversational_history -- list
+
+  """
+  sequences = pipeline(
     prompt,
     do_sample=True,
     top_k=10,
     num_return_sequences=1,
     eos_token_id=tokenizer.eos_token_id,
-    max_length=500,
-)
-for seq in sequences:
-    print(f"Result: {seq['generated_text']}")
+    max_length=250,
+    )
+
+  print('Model Output: ',sequences)
+  conversation_history.append('Bot1: ',sequences[-1]['generated_text'])
+  print('Conversation Output: ',conversation_history)
+
+  return sequences
+
+conversation_history=[initial_prompt]
+
+generate(conversation_history, initial_prompt)
+
+
+# for seq in sequences:
+#     conversation_history=[initial_prompt]
+#     conversation_history.append('Bot1: ',seq['generated_text'])
+
+#     print(f"Result: {seq['generated_text']}")
