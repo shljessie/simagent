14d46c77654b421c9c74fd56a622033a95737bfc
shljessie <sl994@cornell.edu>
2023-11-11 16:32:51 -0800

    multiagent caht

--- chat/test.py
+++ chat/test.py
@@ -1,80 +1,45 @@
-from transformers import (
-    AutoModelForCausalLM, 
-    AutoTokenizer, 
-)
-import transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 import torch
 
+# Initialize model and tokenizer
 model_id = "../Llama-2-7b-chat-hf"
 HF_ACCESS_TOKEN = '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'
 
 model = AutoModelForCausalLM.from_pretrained(model_id, use_auth_token=HF_ACCESS_TOKEN, torch_dtype=torch.float16, device_map="auto")
 tokenizer = AutoTokenizer.from_pretrained(model_id, use_auth_token=HF_ACCESS_TOKEN)
-pipeline = transformers.pipeline(
+chat_pipeline = pipeline(
     "text-generation",
     model=model,
-    tokenizer = tokenizer,
+    tokenizer=tokenizer,
     torch_dtype=torch.float32,
     device_map="auto",
-    token = HF_ACCESS_TOKEN
+    token=HF_ACCESS_TOKEN
 )
 
-
-initial_prompt = """
-
-<s>[INST] <<SYS>>
-You are the Persona. Always act as you are the persona.
-Persona:  My name is Jack i like to party.  my major is business. i am in college.
-<</SYS>>
-
-What is your name? [/INST]
-
-"""
-#initial sequence
-sequences = pipeline(
-    initial_prompt,
-    do_sample=True,
-    top_k=10,
-    num_return_sequences=1,
-    eos_token_id=tokenizer.eos_token_id,
-    max_length=250,
-)
-
-def generate(conversational_history, prompt):
-  """Take in convo history and generate response to last utterance
-
-  conversational_history -- list
-
-  """
-  sequences = pipeline(
-    prompt,
-    do_sample=True,
-    top_k=10,
-    num_return_sequences=1,
-    eos_token_id=tokenizer.eos_token_id,
-    max_length=250,
-    )
-
-  print('Model Output: ',sequences)
-  for seq in sequences:
-    response = seq['generated_text']
+# Function to generate a response
+def generate_response(prompt):
+    sequences = chat_pipeline(prompt, do_sample=True, top_k=10, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id, max_length=250)
+    response = sequences[0]['generated_text']
     marker = "[/INST]\n\n"
     index = response.find(marker)
     if index != -1:
-      result = response[index + len(marker):]
+        return response[index + len(marker):].strip()
+    return response.strip()
 
-    conversation_history.append(f"Bot1:  {result}")
-  print('Conversation Output: ',conversation_history)
+# Initial prompt
+initial_prompt = "What is your name?"
 
-  return sequences
+# Simulate conversation
+conversation_history = [initial_prompt]
+for _ in range(5):  # Number of exchanges
+    bot1_prompt = conversation_history[-1]
+    bot1_response = generate_response(bot1_prompt)
+    conversation_history.append(f"Bot1: {bot1_response}")
 
-conversation_history=[initial_prompt]
+    bot2_prompt = bot1_response
+    bot2_response = generate_response(bot2_prompt)
+    conversation_history.append(f"Bot2: {bot2_response}")
 
-generate(conversation_history, initial_prompt)
-
-
-# for seq in sequences:
-#     conversation_history=[initial_prompt]
-#     conversation_history.append('Bot1: ',seq['generated_text'])
-
-#     print(f"Result: {seq['generated_text']}")
+# Print conversation
+for line in conversation_history:
+    print(line)
