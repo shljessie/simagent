06f6b9e762a97cc11d483ee0ab040e0ed535a147
shljessie <sl994@cornell.edu>
2023-11-12 08:32:59 -0800

    update automatic bot convo

---
+++ chat/chatchat4.py
@@ -0,0 +1,147 @@
+import os
+import dotenv
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from typing import List, Tuple
+
+MAX_INPUT_TOKEN_LENGTH = int(os.getenv("MAX_INPUT_TOKEN_LENGTH", "4096"))
+# Define the bot's persona
+BOT_PERSONA = """
+[SYSTEM]
+I am Rohan a grad student at Stanford studying Material Science. I like cocoalmond spread.
+[/SYSTEM]
+"""
+
+# Define the bot's persona
+BOT2_PERSONA = """
+[SYSTEM]
+I am Seonghee a grad student at Stanford studying Computer Science. I like cilantro.
+[/SYSTEM]
+"""
+
+# Load environment variables and model
+if torch.cuda.is_available():
+    model_id = "../Llama-2-7b-chat-hf"
+    dotenv.load_dotenv('../.env')
+    HF_ACCESS_TOKEN = os.getenv('HF_ACCESS_TOKEN')
+    model = AutoModelForCausalLM.from_pretrained(model_id, use_auth_token=HF_ACCESS_TOKEN, torch_dtype=torch.float16, device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained(model_id, use_auth_token=HF_ACCESS_TOKEN)
+    tokenizer.use_default_system_prompt = False
+
+#generate the chat messages
+def generate(
+    message: str,
+    chat_history: List[Tuple[str, str]],
+    system_prompt: str,
+    max_new_tokens: int = 1024,
+    temperature: float = 0.6,
+    top_p: float = 0.9,
+    top_k: int = 50,
+    repetition_penalty: float = 1.2,
+) -> str:
+    conversation = []
+    # Add the bot's persona to the system prompt
+    full_system_prompt = BOT_PERSONA + (system_prompt if system_prompt else "")
+    conversation.append({"role": "system", "content": full_system_prompt})
+
+    for user, assistant in chat_history:
+        conversation.extend([{"role": "user", "content": user}, {"role": "assistant", "content": assistant}])
+    conversation.append({"role": "user", "content": message})
+
+
+    input_ids = tokenizer.apply_chat_template(conversation, return_tensors="pt")
+    if input_ids.shape[1] > MAX_INPUT_TOKEN_LENGTH:
+        input_ids = input_ids[:, -MAX_INPUT_TOKEN_LENGTH:]
+        print(f"Trimmed input from conversation as it was longer than {MAX_INPUT_TOKEN_LENGTH} tokens.")
+    input_ids = input_ids.to(model.device)
+
+    output = model.generate(
+        input_ids,
+        max_new_tokens=max_new_tokens,
+        do_sample=True,
+        top_p=top_p,
+        top_k=top_k,
+        temperature=temperature,
+        num_beams=1,
+        repetition_penalty=repetition_penalty,
+    )
+
+    # Decode only the last part of the output
+    decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
+    last_response = decoded_output.split(conversation[-1]["content"])[-1].strip()
+    # Remove [/INST] tokens
+    cleaned_response = last_response.replace("[/INST]", "").strip()
+
+    return cleaned_response
+
+
+#generate the chat messages
+def generate_bot2(
+    message: str,
+    chat_history: List[Tuple[str, str]],
+    system_prompt: str,
+    max_new_tokens: int = 1024,
+    temperature: float = 0.6,
+    top_p: float = 0.9,
+    top_k: int = 50,
+    repetition_penalty: float = 1.2,
+) -> str:
+    conversation = []
+    # Add the bot's persona to the system prompt
+    full_system_prompt = BOT2_PERSONA + (system_prompt if system_prompt else "")
+    conversation.append({"role": "system", "content": full_system_prompt})
+
+    for user, assistant in chat_history:
+        conversation.extend([{"role": "user", "content": user}, {"role": "assistant", "content": assistant}])
+    conversation.append({"role": "user", "content": message})
+
+
+    input_ids = tokenizer.apply_chat_template(conversation, return_tensors="pt")
+    if input_ids.shape[1] > MAX_INPUT_TOKEN_LENGTH:
+        input_ids = input_ids[:, -MAX_INPUT_TOKEN_LENGTH:]
+        print(f"Trimmed input from conversation as it was longer than {MAX_INPUT_TOKEN_LENGTH} tokens.")
+    input_ids = input_ids.to(model.device)
+
+    output = model.generate(
+        input_ids,
+        max_new_tokens=max_new_tokens,
+        do_sample=True,
+        top_p=top_p,
+        top_k=top_k,
+        temperature=temperature,
+        num_beams=1,
+        repetition_penalty=repetition_penalty,
+    )
+
+    # Decode only the last part of the output
+    decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
+    last_response = decoded_output.split(conversation[-1]["content"])[-1].strip()
+    # Remove [/INST] tokens
+    cleaned_response = last_response.replace("[/INST]", "").strip()
+
+    return cleaned_response
+
+
+
+if __name__ == "__main__":
+    # Initialize chat history with the bot's persona
+    initial_bot1_message = "I am Rohan, a grad student at Stanford studying Material Science. I like cocoa almond spread."
+    initial_bot2_message = "I am Seonghee a grad student at Stanford studying Computer Science. I like cilantro."
+    chat_history = [("Bot1 Persona", initial_bot1_message), ("Bot2 Persona", initial_bot2_message)]
+
+    rounds = 5  # Number of conversational rounds
+    for _ in range(rounds):
+        # Bot1 generates a response
+        bot1_response = generate(initial_bot1_message, chat_history, system_prompt="", max_new_tokens=1024)
+        chat_history.append(("Bot1", bot1_response))
+        print("Bot1:", bot1_response)
+
+        # Bot2 generates a response
+        bot2_response = generate_bot2(bot1_response, chat_history, system_prompt="", max_new_tokens=1024)
+        chat_history.append(("Bot2", bot2_response))
+        print("Bot2:", bot2_response)
+
+    # Print the chat history
+    print("\n----- Conversation History -----")
+    for sender, msg in chat_history:
+        print(f"{sender}: {msg}")
