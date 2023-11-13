49825ab347de377c9972a1f0a321d5e1d7d5206f
shljessie <sl994@cornell.edu>
2023-11-13 06:26:08 -0800

    udpate empty line bug

--- chat/multiagent_chat.py
+++ chat/multiagent_chat.py
@@ -74,7 +74,15 @@ def generate(
 
     # Decode only the last part of the output
     decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
-    last_response = decoded_output.split(conversation[-1]["content"])[-1].strip()
+    # last_response = decoded_output.split(conversation[-1]["content"])[-1].strip()
+        # last_response = decoded_output.split(conversation[-1]["content"])[-1].strip()
+    
+    if conversation[-1]["content"]:
+        # Only split if the content is not empty
+        last_response = decoded_output.split(conversation[-1]["content"])[-1].strip()
+    else:
+        # Handle the case where there is no content to split by
+        last_response = decoded_output.strip()
     # Remove [/INST] tokens
     cleaned_response = last_response.replace("[/INST]", "").strip()
 
@@ -121,7 +129,15 @@ def generate_bot2(
 
     # Decode only the last part of the output
     decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
-    last_response = decoded_output.split(conversation[-1]["content"])[-1].strip()
+    # last_response = decoded_output.split(conversation[-1]["content"])[-1].strip()
+        # last_response = decoded_output.split(conversation[-1]["content"])[-1].strip()
+    
+    if conversation[-1]["content"]:
+        # Only split if the content is not empty
+        last_response = decoded_output.split(conversation[-1]["content"])[-1].strip()
+    else:
+        # Handle the case where there is no content to split by
+        last_response = decoded_output.strip()
     # Remove [/INST] tokens
     cleaned_response = last_response.replace("[/INST]", "").strip()
 
