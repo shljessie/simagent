4f87aab6e6e04f3ec17ca66386f6c75ce2717118
shljessie <sl994@cornell.edu>
2023-11-14 11:06:23 -0800

    update prompt

--- chat/multiagent_chat.py
+++ chat/multiagent_chat.py
@@ -164,6 +164,7 @@ if __name__ == "__main__":
 
     # Set the initial response for the first round, start with bot2
     bot2_initial_response = generate_bot2("", chat_history , system_prompt=BOT2_PERSONA, max_new_tokens=200)
+    print('\n Initial Bot2 Response: ', bot2_initial_response, "\n")
     chat_history.append((initial_bot1_message, bot2_initial_response))
     csv_data.append(("Bot2: ",bot2_initial_response))
 
