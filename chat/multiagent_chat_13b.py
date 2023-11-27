9b961709929d97a075e3a68b3b37247498a8ee45
shljessie <sl994@cornell.edu>
2023-11-26 19:47:36 -0800

    add encoding

--- chat/multiagent_chat_13b.py
+++ chat/multiagent_chat_13b.py
@@ -230,7 +230,7 @@ if __name__ == "__main__":
     csv_file = "conversation_data.csv"
     csv_columns = ['Conversation History', 'Diagnostic Question', 'Bot1 Response', 'Ground Truth Answer', 'Loss']
     try:
-        with open(csv_file, 'w', newline='') as csvfile:
+        with open(csv_file, 'w', newline='', encoding='utf-8') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
             writer.writeheader()
             print(csv_data)
