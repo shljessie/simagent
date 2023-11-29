4406c1b51aba9160037a929bff09b1b7b52979f3
shljessie <sl994@cornell.edu>
2023-11-29 09:51:27 -0800

    revert to original

--- chat/diagnostic2.py
+++ chat/diagnostic2.py
@@ -4,13 +4,9 @@ from torch.nn import CrossEntropyLoss
 
 BOT_PERSONA = """
 [SYSTEM]
-You are Rohan a grad student at Stanford studying Material Science. I like cocoalmond spread.
+You are Rohan a grad student at Stanford studying Material Science. You are 22 years old. Respond with one sentence only.
 [/SYSTEM]
-Limit your response to one sentence.
-Respond in this format 
-
-What is your xx?
-My xx is 
+Respond with one sentence only.
 """
 
 @torch.no_grad()
@@ -48,45 +44,34 @@ def calculate_loss(model, tokenizer, convo_history, bot1_diag_response, ground_t
     bot1_diag_response = tokenizer(bot1_diag_response, return_tensors='pt')["input_ids"].to(device)
     ground_truth_answers = tokenizer(ground_truth_answers, return_tensors='pt')["input_ids"].to(device)
     diag_question_response = torch.concat([inputs, bot1_diag_response], dim=-1).to(device) # add tensors together
-    cat_ground_truth_answers =  torch.concat([inputs, ground_truth_answers], dim=-1).to(device) # add tensors together
 
 
-    # # Check token lengths
-    # print("Input tokens length:", inputs.size(), "\n")
-    # print("Bot1 response tokens length:", bot1_diag_response.size(), "\n")
-    # print("Ground truth tokens length:", ground_truth_answers.size(), "\n")
-    # print("Concat size: ", diag_question_response.size(), "\n")
+    # Check token lengths
+    print("Input tokens length:", inputs.size(), "\n")
+    print("Bot1 response tokens length:", bot1_diag_response.size(), "\n")
+    print("Ground truth tokens length:", ground_truth_answers.size(), "\n")
+    print("Concat size: ", diag_question_response.size(), "\n")
     
-    # # check tokenized inputs
-    # check = tokenizer.decode(diag_question_response[0])
-    # print("Check decoded tokenizer: \n", check, "\n")
+    # check tokenized inputs
+    check = tokenizer.decode(diag_question_response[0])
+    print("Check decoded tokenizer: \n", check, "\n")
 
     # pass through model, get hidden state
     outputs = model(diag_question_response, output_hidden_states=True) 
     # get hidden state of response to diagnostic output
     hiddens_diag_response = outputs.hidden_states[-1][:, -1+(-1*bot1_diag_response.shape[-1]):-1]
-    hiddens_cat_ground_truth_answers = outputs.hidden_states[-1][:, -1+(-1*ground_truth_answers.shape[-1]):-1]
-    print("hiddens_diag_response zie: ", hiddens_diag_response.size(), "\n")
-    print("hiddens_cat_diag_response zie: ", hiddens_cat_ground_truth_answers.size(), "\n")
+    print("hiddens_diag_response size: ", hiddens_diag_response.size(), "\n")
 
     # calculate loss
     logits  = model.lm_head(hiddens_diag_response) #compare model output against actual tokens
-    logits_truth = model.lm_head(hiddens_cat_ground_truth_answers)
-    max_length = max(logits.shape[1], logits_truth.shape[1])
-    if logits.shape[1] < max_length:
-        # Padding to the right (at the end of each sequence)
-        logits = torch.nn.functional.pad(logits, (0, 0, 0, max_length - logits.shape[1]))
-    elif logits_truth.shape[1] < max_length:
-        logits_truth = torch.nn.functional.pad(logits_truth, (0, 0, 0, max_length - logits_truth.shape[1]))
-        
-    # padded_ground_truth_answers = torch.nn.functional.pad(
-    #     ground_truth_answers, (0, bot1_diag_response.size(1) - ground_truth_answers.size(1)), 'constant', 0
-    # )
-    # logits = logits[:, :padded_ground_truth_answers.size(1), :].contiguous()
-    
+    padded_ground_truth_answers = torch.nn.functional.pad(
+        ground_truth_answers, (0, bot1_diag_response.size(1) - ground_truth_answers.size(1)), 'constant', 0
+    )
+    logits = logits[:, :padded_ground_truth_answers.size(1), :].contiguous()
     loss_fct = CrossEntropyLoss(reduction="mean")
-    loss = loss_fct(logits.view(-1, logits.size(-1)), logits_truth.view(-1, logits.size(-1)))
-    # loss = loss_fct(logits.view(-1, logits.size(-1)), padded_ground_truth_answers.view(-1))
+    print('final logits:', logits.view(-1, logits.size(-1)))
+    print('final ground truth,', padded_ground_truth_answers.view(-1) )
+    loss = loss_fct(logits.view(-1, logits.size(-1)), padded_ground_truth_answers.view(-1))
 
     # Q: should we append the ground truth answers too?
     return loss.item(),conversation
