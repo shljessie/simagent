0ba57758c61c9252510c4bac1acbaecbe39de590
shljessie <sl994@cornell.edu>
2023-12-13 01:33:46 +0300

    update diagnostic loss

--- chat/diagnostic_backprop.py
+++ chat/diagnostic_backprop.py
@@ -1,35 +1,16 @@
-import os
-import dotenv
+# diagnostic.py
 import torch
 from torch.nn import CrossEntropyLoss
-from transformers import (
-    AutoConfig, 
-    AutoModelForCausalLM, 
-    AutoTokenizer, 
-    BitsAndBytesConfig, 
-)
+import torch.nn.functional as F
 
-if torch.cuda.is_available():
-    # Get the current GPU's index
-    current_device = torch.cuda.current_device()
+BOT_PERSONA = """
+[SYSTEM]
+You are Rohan a grad student at Stanford studying Material Science. You are 22 years old.
+[SYSTEM]
+Respond with one sentence only.
+"""
 
-    # Get the name of the current GPU
-    torch.cuda.get_device_name(current_device)
-
-    # Total memory
-    total_memory = torch.cuda.get_device_properties(current_device).total_memory
-    print(f"Total memory: {total_memory / 1e9} GB")
-
-    # Allocated memory
-    allocated_memory = torch.cuda.memory_allocated(current_device)
-    print(f"Allocated memory: {allocated_memory / 1e9} GB")
-
-    # Cached memory
-    cached_memory = torch.cuda.memory_reserved(current_device)
-    print(f"Cached memory: {cached_memory / 1e9} GB")
-    
-# Function to calculate loss
-def calculate_loss(model, tokenizer, convo_history, bot1_diag_response, ground_truth_answers):
+def calculate_loss(model, tokenizer, convo_history, bot1_diag_response, ground_truth_answers, diagnostic_question):
     """Calculate the cross entropy loss of the diagnostic responses and ground_truth answers.
     This loss is calculated for each diagnostic question.
 
@@ -43,30 +24,40 @@ def calculate_loss(model, tokenizer, convo_history, bot1_diag_response, ground_t
     """
 
     # check model inputs
-    print("Calculate Loss \n")
-    print("Conversation History: \n", convo_history )
+    print("------------------------ Calculating Loss ----------------------")
+    print("Conversation History: \n", convo_history, "\n" )
     print("Bot1 Diagnostic Response: \n", bot1_diag_response)
     print("Ground Truth Answers: \n", ground_truth_answers, "\n")
 
+    conversation = []
+    full_system_prompt = BOT_PERSONA
+    conversation.append({"role": "system", "content": full_system_prompt})
+
+    for user, assistant in convo_history:
+        conversation.extend([{"role": "user", "content": user}, {"role": "assistant", "content": assistant}])
+
+    conversation.append({"role":"user", "content":diagnostic_question})
+    print('\nHF Conversation passed through chat_history in: ', conversation, "\n")
+
     # tokenize inputs
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    inputs = tokenizer(convo_history, return_tensors='pt')["input_ids"].to(device)
-    bot1_diag_response = tokenizer(bot1_diag_response, return_tensors='pt')["input_ids"].to(device)
+    conversation_token = tokenizer.apply_chat_template(conversation, return_tensors="pt").to(device)
     ground_truth_answers = tokenizer(ground_truth_answers, return_tensors='pt')["input_ids"].to(device)
-    diag_question_response = torch.concat([inputs, bot1_diag_response], dim=-1).to(device)
-
-    # check tokenized inputs
-    check = tokenizer.decode(diag_question_response[0])
-    print("Check decoded tokenizer: \n", check, "\n")
+    concat_history_truth = torch.concat([conversation_token, ground_truth_answers], dim=-1).to(device)  
 
     # pass through model, get hidden state
-    outputs = model(diag_question_response, output_hidden_states=True) 
-    # get hidden state of response to diagnostic output
-    hiddens_diag_response = outputs.hidden_states[-1][:, -1+(-1*bot1_diag_response.shape[-1]):-1]
+    outputs = model(concat_history_truth, output_hidden_states=True) 
+    #check hidden state shape 
+    hiddens_diag_response = outputs.hidden_states[-1][:, -1+(-1*ground_truth_answers.shape[-1]):-1]
+
+    logits  = model.lm_head(hiddens_diag_response)
 
     # calculate loss
-    logits  = model.lm_head(hiddens_diag_response) #compare model output against actual tokens
     loss_fct = CrossEntropyLoss(reduction="mean")
-    loss = loss_fct(logits.squeeze(), ground_truth_answers.squeeze()) # get the logits probabilities bot1_diag_response and ground_truth answers
+    print('Logits Shape: ', logits.view(-1, logits.size(-1)).shape)
+    print('Ground Truth Answers Shape: ', ground_truth_answers.view(-1).shape)
+    loss = loss_fct(logits.view(-1, logits.size(-1)),ground_truth_answers.view(-1)) # (n,c) n shape required
 
-    return loss.item()
+    print('Loss Calculation', loss)
+
+    return loss.item(), conversation
