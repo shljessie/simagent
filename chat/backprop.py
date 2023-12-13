a0f5ede68f8743eb78c74ba0fb0deb6d31398c96
shljessie <sl994@cornell.edu>
2023-12-14 01:55:12 +0300

    update backprop

--- chat/backprop.py
+++ chat/backprop.py
@@ -6,10 +6,10 @@ from typing import List, Tuple
 from diagnostic_backprop import calculate_loss
 from torch.optim import Adam
 import csv
-from torch.utils.data import DataLoader, TensorDataset
 
-predefined_questions = ["What is your name?", "How old are you?", "What is your major?"]
-true_answers = ["My name is Rohan", "I am 22 years old", "My major is Material Science"]
+predefined_questions = ["What is you name?", "How old are you?", "What is your major?"]
+
+true_answers = ["My name is Rohan","I am 22 years old","My major is Material Science"]
 
 BOT_PERSONA = """
 [SYSTEM]
@@ -30,34 +30,34 @@ if torch.cuda.is_available():
     dotenv.load_dotenv('../.env')
     HF_ACCESS_TOKEN = os.getenv('HF_ACCESS_TOKEN')
     model = AutoModelForCausalLM.from_pretrained(model_id, use_auth_token=HF_ACCESS_TOKEN, torch_dtype=torch.float16, device_map="auto")
-    model.bfloat16()
     tokenizer = AutoTokenizer.from_pretrained(model_id, use_auth_token=HF_ACCESS_TOKEN)
+    model.bfloat16()
     tokenizer.use_default_system_prompt = False
-    optimizer = Adam(model.parameters(), lr=0.001)
-    model.train()
+    optimizer = Adam(model.parameters(), lr=0.001) 
 
 MAX_INPUT_TOKEN_LENGTH = int(os.getenv("MAX_INPUT_TOKEN_LENGTH", "400"))
 
-def generate_batched(
-    messages: List[str],
+def generate(
+    message: str,
     chat_history: List[Tuple[str, str]],
     system_prompt: str,
     max_new_tokens: int = 10,
-    temperature: float = 0.8,
+    temperature: float = 0.6,
     top_p: float = 0.9,
     top_k: int = 50,
     repetition_penalty: float = 1.2,
-) -> List[str]:
+) -> str:
+
     conversation = []
 
     full_system_prompt = (system_prompt if system_prompt else "")
     conversation.append({"role": "system", "content": full_system_prompt})
 
+    print('--------- Bot1 ----------')
+
     for user, assistant in chat_history:
         conversation.extend([{"role": "user", "content": user}, {"role": "assistant", "content": assistant}])
-
-    for message in messages:
-        conversation.append({"role": "user", "content": message})
+    conversation.append({"role": "user", "content": message})
 
     input_ids = tokenizer.apply_chat_template(conversation, return_tensors="pt")
     input_ids = input_ids.to(model.device)
@@ -73,67 +73,120 @@ def generate_batched(
         repetition_penalty=repetition_penalty,
     )
 
-    decoded_outputs = [tokenizer.decode(output[i], skip_special_tokens=True) for i in range(len(messages))]
+    decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
 
-    cleaned_responses = [output.split("[/INST]")[0].strip() for output in decoded_outputs]
+    if conversation[-1]["content"]:
+        last_response = decoded_output.split(conversation[-1]["content"])[-1].strip()
+    else:
+        last_response = decoded_output.strip()
 
-    return cleaned_responses
+    cleaned_response = last_response.replace("[/INST]", "").strip()
+
+    return cleaned_response
+
+def generate_bot2(
+    message: str,
+    chat_history: List[Tuple[str, str]],
+    system_prompt: str,
+    max_new_tokens: int = 10,
+    temperature: float = 0.6,
+    top_p: float = 0.9,
+    top_k: int = 50,
+    repetition_penalty: float = 1.2,
+) -> str:
+    conversation = []
+    full_system_prompt = (system_prompt if system_prompt else "")
+    conversation.append({"role": "system", "content": full_system_prompt})
+
+    for user, assistant in chat_history:
+        conversation.extend([{"role": "user", "content": user}, {"role": "assistant", "content": assistant}])
+    conversation.append({"role": "user", "content": message})
+
+    input_ids = tokenizer.apply_chat_template(conversation, return_tensors="pt")
+    input_ids = input_ids.to(model.device)
+
+    output = model.generate(
+        input_ids,
+        max_new_tokens=max_new_tokens,
+        do_sample=True,
+        top_p=top_p,
+        top_k=top_k,
+        temperature=temperature,
+        num_beams=1,
+        repetition_penalty=repetition_penalty,
+    )
+
+
+    decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
+    
+    if conversation[-1]["content"]:
+        last_response = decoded_output.split(conversation[-1]["content"])[-1].strip()
+    else:
+        # Handle the case where there is no content to split by
+        last_response = decoded_output.strip()
+    # Remove [/INST] tokens
+    cleaned_response = last_response.replace("[/INST]", "").strip()
+
+    return cleaned_response
+
 
 if __name__ == "__main__":
-    initial_bot1_message = "You are Rohan a grad student at Stanford studying Material Science. You are 22 years old."
+    # Initialize chat history with the bot's personas
+    initial_bot1_message = "You are a student learning fractions. You have some misconceptions. When you add fractions, you add the numerators and denominators together.For example, you believe that  1/2+1/3 = 1/5."
     initial_bot2_message = "I am Seonghee, a grad student at Stanford studying Computer Science. I like cilantro."
     chat_history_bot1 = []
     chat_history_bot2 = []
-    csv_data = []
+    csv_data = [] 
 
-    last_response = generate_batched(["Hello! What is your name?"], chat_history_bot2 , system_prompt=BOT2_PERSONA, max_new_tokens=30)
-    chat_history_bot2.append((initial_bot1_message, last_response[0]))
+    # Set the initial response for the first round, start with bot2
+    last_response = generate_bot2("Hello! What is your name?", chat_history_bot2 , system_prompt=BOT2_PERSONA, max_new_tokens=30)
+    chat_history_bot2.append((initial_bot1_message, last_response))
 
-    rounds = 30
+    rounds = 30  # Number of conversational rounds
     for _ in range(rounds):
-        bot1_responses = generate_batched([last_response[0]], chat_history_bot1, system_prompt=BOT_PERSONA, max_new_tokens=30)
-        chat_history_bot1.append((last_response[0], bot1_responses[0]))
+        # Bot1 generates a response to Bot2's last message
+        bot1_response = generate(last_response, chat_history_bot1, system_prompt=BOT_PERSONA, max_new_tokens=30)
+        chat_history_bot1.append((last_response, bot1_response))
 
-        print("Bot1:", bot1_responses[0])
+        print("Bot1:", bot1_response)
         print("\n--------------------------------------------------\n")
-
-        # Diagnostic Question
+        
+        #Diagnostic Question
         for i in range(len(predefined_questions)):
-            bot1_diag_responses = generate_batched([predefined_questions[i]], chat_history_bot1, system_prompt=BOT_PERSONA, max_new_tokens=30)
-
-            # Assuming calculate_loss returns a list of losses
-            losses = calculate_loss(model, tokenizer, chat_history_bot1, bot1_diag_responses[0], true_answers[i], predefined_questions[i])
-
-            csv_data.append({
+          # place a diagnostic question
+          bot1_diag_response = generate(predefined_questions[i], chat_history_bot1, system_prompt=BOT_PERSONA, max_new_tokens=30 )  
+          #calculate loss
+          loss, conversation = calculate_loss(model, tokenizer, chat_history_bot1, bot1_diag_response, true_answers[i], predefined_questions[i] )
+          csv_data.append({
+                # 'Conversation History': conversation,
                 'Diagnostic Question': predefined_questions[i],
-                'Bot1 Response': bot1_diag_responses[0],
+                'Bot1 Response': bot1_diag_response,
                 'Ground Truth Answer': true_answers[i],
-                'Loss': float(losses[0].item()),  # You may need to adjust this part based on your loss calculation
+                'Loss': int(loss),
             })
-
-            optimizer.zero_grad()
-            losses[0].backward()
-            optimizer.step()
-            print('Backward Step Complete')
+          optimizer.zero_grad()  # Clear existing gradients
+          loss.backward()        # Compute gradients of all variables wrt loss
+          optimizer.step()  
 
         print("\n--------------------------------------------------\n")
+        
+        # Bot2 generates a response to Bot1's last message
+        bot2_response = generate_bot2(bot1_response, chat_history_bot2, system_prompt=BOT2_PERSONA, max_new_tokens=30)
+        chat_history_bot2.append((bot1_response, bot2_response))
 
-        bot2_responses = generate_batched([bot1_responses[0]], chat_history_bot2, system_prompt=BOT2_PERSONA, max_new_tokens=30)
-        chat_history_bot2.append((bot1_responses[0], bot2_responses[0]))
-
-        print("Bot2:", bot2_responses[0])
+        print("Bot2:", bot2_response)
         print("\n--------------------------------------------------\n")
 
-        last_response = bot2_responses[0]
+        # Update the last response
+        last_response = bot2_response
+
 
     print('CSV_____________________')
-
     def clean_string(s):
         return s.encode('ascii', 'ignore').decode('ascii')
-
-    csv_file = f"loss_7b_backprop.csv"
+    csv_file =f"loss_7b.csv"
+    # csv_columns = ['Conversation History', 'Diagnostic Question', 'Bot1 Response', 'Ground Truth Answer', 'Loss']
     csv_columns = ['Diagnostic Question', 'Bot1 Response', 'Ground Truth Answer', 'Loss']
-
     try:
         with open(csv_file, 'w', newline='', encoding='utf-8') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
@@ -149,5 +202,4 @@ if __name__ == "__main__":
     except IOError:
         print("I/O error while writing to CSV")
 
-    # Save the trained model (optional)
-    model.save_pretrained("./backprop_llama2.py")
+    
