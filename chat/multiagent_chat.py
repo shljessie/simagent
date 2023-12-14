efeeae4702978e0e5c58b226a7cb6c85f07a291f
shljessie <sl994@cornell.edu>
2023-12-14 03:12:29 +0300

    use llama2 tokenizer

--- chat/multiagent_chat.py
+++ chat/multiagent_chat.py
@@ -34,10 +34,11 @@ if not torch.cuda.is_available():
 # Load environment variables and model
 if torch.cuda.is_available():
     model_id = "backprop_llama2"
+    tokenizer_id="../Llama-2-7b-chat-hf"
     dotenv.load_dotenv('../.env')
     HF_ACCESS_TOKEN = os.getenv('HF_ACCESS_TOKEN')
     model = AutoModelForCausalLM.from_pretrained(model_id, use_auth_token=HF_ACCESS_TOKEN, torch_dtype=torch.float16, device_map="auto")
-    tokenizer = AutoTokenizer.from_pretrained(model_id, use_auth_token=HF_ACCESS_TOKEN)
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_id, use_auth_token=HF_ACCESS_TOKEN)
     tokenizer.use_default_system_prompt = False
 
 @torch.no_grad()
