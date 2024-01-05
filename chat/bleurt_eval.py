35bc829040379aee7c09254c8c179ccb8486ae71
shljessie <sl994@cornell.edu>
2024-01-05 09:39:28 +0300

    update file path

--- chat/bleurt_eval.py
+++ chat/bleurt_eval.py
@@ -4,7 +4,7 @@ import os
 from bleurt import score
 
 def bleurt(input_csv, output_csv):
-    checkpoint = "bleurt/test_checkpoint"
+    checkpoint = "/bluert/bleurt/test_checkpoint"
     scorer = score.BleurtScorer(checkpoint)
 
     with open(input_csv, newline='', encoding='utf-8') as infile, \
